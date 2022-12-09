1d4d695a991418d73b10267046dc9cb7744b65f5
Luccavco <111525582+Luccavco@users.noreply.github.com>
2022-12-09 20:03:06 -0300

    Add files via upload
    felipao o salva-vidas

--- nebas.asm
+++ nebas.asm
@@ -1,34 +1,116 @@
-TITLE Nome:Jean Pyton RA:22013310
+TITLE Lucca/22003004        Fernando/
 .MODEL SMALL
-.DATA
-    linha DB 9
-    coluna DB 9
-    offsetlinha DW 18
-    
-    MATRIZ  DW ?,?,?,?,?,?,?,?,?   ;matriz
-            DW ?,?,?,?,?,?,?,?,?
-            DW ?,?,?,?,?,?,?,?,?
-            DW ?,?,?,?,?,?,?,?,?
-            DW ?,?,?,?,?,?,?,?,?
-            DW ?,?,?,?,?,?,?,?,?
-            DW ?,?,?,?,?,?,?,?,?
-            DW ?,?,?,?,?,?,?,?,?
-            DW ?,?,?,?,?,?,?,?,?
 
-    MATRIZ2 DW ?,?,?,?,?,?,?,?,?   ;matriz auxiliar
-            DW ?,?,?,?,?,?,?,?,?
-            DW ?,?,?,?,?,?,?,?,?
-            DW ?,?,?,?,?,?,?,?,?
-            DW ?,?,?,?,?,?,?,?,?
-            DW ?,?,?,?,?,?,?,?,?
-            DW ?,?,?,?,?,?,?,?,?
-            DW ?,?,?,?,?,?,?,?,?
-            DW ?,?,?,?,?,?,?,?,?
+PULO  MACRO 
+    mov ah,02
+    mov dl,10
+    int 21H
+ENDM
+
+PUSHR  MACRO 
+    push si
+    push dx
+    push cx
+    push bx
+    push ax   
+ENDM 
+
+POPR  MACRO 
+    pop si
+    pop dx
+    pop cx
+    pop bx
+    pop ax 
+ENDM
+
+apagatela MACRO
+    MOV AH,0h
+    MOV AL,3h
+    INT 10H
+    ;XOR AX,AX
+    MOV AH,09
+    MOV AL,20H
+    MOV BH,00
+    MOV BL,3FH
+    MOV CX,800H
+    INT 10H
+ENDM
+
+LIMPAR MACRO
+    XOR AX,AX
+    XOR BX,BX
+    XOR CX,CX
+    XOR DX,DX
+    XOR SI,SI
+ENDM
+
+PRINTM MACRO print
+    MOV AH,09
+    LEA DX,print
+    INT 21H
+ENDM
+
+space MACRO 
+    MOV AH, 02
+    MOV DL, 32
+    INT 21H    
+ENDM
+
+.DATA
+
+    ;home DB 
+
+    LINHA DW 9
+    COLUNA DW 9
+    
+    
+    MATRIZ1  DB 38H,?,36H,?,?,33H,?,39H,?   ;matriz
+             DB ?,34H,?,31H,?,?,?,36H,38H
+             DB 32H,?,?,38H,37H,?,?,?,35H
+             DB 31H,?,38H,?,?,35H,?,32H,?
+             DB ?,33H,?,31H,?,?,?,35H,?
+             DB 37H,?,35H,?,33H,?,39H,?,?
+             DB ?,32H,31H,?,?,37H,?,34H,?
+             DB 36H,?,?,?,32H,?,38H,?,?
+             DB ?,38H,37H,36H,?,34H,?,?,33H
+
+    MATRIZ1R DB 8,7,6,5,4,3,1,9,2   ;matriz
+             DB 5,4,3,2,1,9,7,6,8
+             DB 2,1,9,8,7,6,4,3,5
+             DB 1,9,8,1,6,5,3,2,4
+             DB 4,3,2,4,9,8,6,5,7
+             DB 7,6,5,4,3,2,9,8,1
+             DB 3,2,1,9,8,7,5,4,6
+             DB 6,5,4,3,2,1,8,7,9
+             DB 9,8,7,6,5,4,2,1,3
+    
+    MATRIZ2  DB ?,?,?,?,?,?,?,?,?   ;matriz
+             DB ?,?,?,?,?,?,?,?,?
+             DB ?,?,?,?,?,?,?,?,?
+             DB ?,?,?,?,?,?,?,?,?
+             DB ?,?,?,?,?,?,?,?,?
+             DB ?,?,?,?,?,?,?,?,?
+             DB ?,?,?,?,?,?,?,?,?
+             DB ?,?,?,?,?,?,?,?,?
+             DB ?,?,?,?,?,?,?,?,?
+
+    MATRIZ2R DB ?,?,?,?,?,?,?,?,?   ;matriz
+             DB ?,?,?,?,?,?,?,?,?
+             DB ?,?,?,?,?,?,?,?,?
+             DB ?,?,?,?,?,?,?,?,?
+             DB ?,?,?,?,?,?,?,?,?
+             DB ?,?,?,?,?,?,?,?,?
+             DB ?,?,?,?,?,?,?,?,?
+             DB ?,?,?,?,?,?,?,?,?
+             DB ?,?,?,?,?,?,?,?,?
 
     msgx DB "<Fase 1>",10,'$'                                         ;idem
     msgy DB "<Fase 2>",10,'$'
-    msg1 DB "Insira os valores da matriz:",10,"$"
-    msg2 DB 10,"Matriz original:",10,"$"
+    msg1 DB "Insira a linha:",10,'$'
+    msg2 DB "Insira a coluna:",10,'$'
+    msg3 DB "Insira o valor:",10,'$'
+    msg4 DB "Voce digitou o valor INCORRETO!",10,'$'
+    msg5 DB "Voce digitou o valor CORRETO!",10,'$'
 
 .CODE
 
@@ -48,7 +130,7 @@ main PROC
     SUB AL,30H              ;realiza subtração para transformar caractere em número
 
     CMP AL,1                ;realiza uma comparação, se o conteúdo em AL=1, realiza um Jump Equal(JE) para a "SOMA"
-    CALL PENIS
+    CALL FASE1
 
     ;CMP AL,2                ;realiza uma comparação, se o conteúdo em AL=2, realiza um Jump Equal(JE) para a "SUBT"
     ;JE PENIS2
@@ -65,67 +147,135 @@ main PROC
     ;INT 21h
 main ENDP
 
-PENIS PROC
+FASE1 PROC
 
-    MOV AH,09
-    LEA DX,msg1
-    INT 21h
-    CALL RECEBA         ;entrada de dados na matriz
-    MOV AH,09
-    LEA DX,msg2         
-    INT 21h
-    CALL PRINT          
-    MOV ah,4Ch
-    INT 21h
+    COMECO:
+
+    LEA BX, MATRIZ1
+    
+    CALL PRINT
+    
+    PRINTM msg1
+
+    MOV AH,01H
+    INT 21H
+    SUB AL,30H
+
+    MOV CX,9
+    MUL CX
+    XOR AH,AH
+
+    MOV BX,AX
+    SUB BX,9
+
+    PRINTM msg2
+
+    MOV AH,01H
+    INT 21H
+    SUB AL,31H
+
+    XOR AH,AH
+    MOV SI,AX
+
+    PRINTM msg3
+
+    MOV AH,01H
+    INT 21H
+
+    SUB AL,30H
+
+    CALL VERIFICA
+    JMP COMECO
+
     RET
-PENIS ENDP
 
-RECEBA PROC
-    XOR SI,SI           ;index
-    MOV CL,linha            ;loop linha
-    MOV CH,coluna            ;loop coluna
-    LOOPlinha:
-    LOOPcol:
-    MOV AH,1h           ;input
-    INT 21h
-    AND AX,00FFh        ;mascara para pegar o apenas o input em AL
-    SUB AX,30h          ;char para numero
-    MOV MATRIZ[SI],AX   ;preenchimento da matriz
-    ADD SI,2            ;index anda de 2 em 2
-    DEC CH
-    JNZ LOOPcol
-    MOV AH,2            ;quebra de linha
-    MOV DL,10
-    INT 21h
-    MOV CH,coluna
-    DEC CL
-    JNZ LOOPlinha
+FASE1 ENDP
+
+VERIFICA PROC
+
+CMP AL,MATRIZ1R[BX][SI]
+
+    JE CORRETO
+    
+    PRINTM msg4     ;VALOR INCORRETO
+
+    MOV AH,01
+    INT 21H
+    JMP SAIDA
+    XOR AX,AX
+    
+    CORRETO:
+    
+    PUSH AX
+
+    PRINTM msg5      ;VALOR CORRETO
+    
+    POP AX
+    ADD AL,30H
+
+    MOV MATRIZ1[BX][SI],AL
+
+    MOV AH,01
+    INT 21H
+
+    SAIDA:
+    
+    LIMPAR
+
+    ;CALL TERMINA_FACIL
+    ;CMP DL,1
+
     RET
-RECEBA ENDP
 
-;description
+VERIFICA ENDP
+
 PRINT PROC
-    MOV CH,coluna                    ;LOOP1
-    MOV CL,linha                    ;LOOP2
-    XOR BX,BX                   ;index linha
-    XOR SI,SI                   ;index coluna
-    MOV AH,2h
-    LOOP2:
-    LOOP1:
-    MOV DX,MATRIZ[BX][SI]       ;elemento a ser impresso
-    ADD DX,30h                  ;numero para char
-    INT 21h
-    ADD SI,2                    ;index coluna anda de 2 em 2
-    DEC CH
-    JNZ LOOP1
-    XOR SI,SI                   ;index coluna reset
-    ADD BX,offsetlinha                    ;index linha vai para proxima linha
-    MOV CH,coluna                    ;loop de coluna reset
-    MOV DL,10                   ;quebra de linha
-    INT 21h
-    DEC CL
-    JNZ LOOP2
+        PULO
+        PUSHR
+
+        MOV AH,02
+        MOV CX,LINHA
+        MOV DX,30H 
+
+
+       EXTERNO:
+            MOV DI,COLUNA
+            XOR SI,SI
+
+        INTERNO:
+
+            FORA:
+
+            CMP BX,0
+            JNE FORA2
+            MOV DL,30H
+            INC DH
+            ADD DL,DH
+            INT 21H
+
+            FORA2:
+
+            MOV DL, [BX][SI]
+            INT 21H
+            INC SI
+            DEC DI
+
+            MOV DL,32
+            INT 21H
+
+            JNZ INTERNO
+
+            PULO
+
+            ADD BX,COLUNA
+
+            LOOP EXTERNO
+            POPR
+
+
     RET
-PRINT ENDP
+    PRINT ENDP
+
+    
 
 end main
